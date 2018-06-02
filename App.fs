f1976ff95b449d9680cff1cf34cecf094983e985
mkolibaba <avierdaner@gmail.com>
2018-06-02 12:35:01 +0300

    Init

---
+++ App.fs
@@ -0,0 +1,24 @@
+open Suave                 // always open suave
+open Suave.Filters
+open Suave.Operators
+open Suave.RequestErrors
+open Suave.Successful      // for OK-result
+open Suave.Web             // for config
+
+let browse =
+    request (fun r ->
+        match r.queryParam "genre" with
+        | Choice1Of2 genre -> OK (sprintf "Genre: %s" genre)
+        | Choice2Of2 msg -> BAD_REQUEST msg)
+
+let webPart =
+    choose [
+        path "/" >=> (OK "Home")
+        path "/store" >=> (OK "Store")
+        path "/store/browse" >=> browse
+        path "/store/details" >=> (OK "Details")
+        pathScan "/store/details/%d"
+            (fun id -> OK (sprintf "Details: %d" id))
+    ]
+
+startWebServer defaultConfig webPart
